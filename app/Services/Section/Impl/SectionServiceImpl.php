9c9ae44b34699d675c37fff3a9b7a37145862629
Omar Alaa <omar.alaa@mansourgroup.com>
2026-06-10 15:01:19 +0300

    make sections redirect - return with product or category name

--- app/Services/Section/Impl/SectionServiceImpl.php
+++ app/Services/Section/Impl/SectionServiceImpl.php
@@ -16,14 +16,30 @@ class SectionServiceImpl implements SectionService
             ->orderBy('sort_order', 'asc')
             ->get()
             ->map(function ($section) {
+                $actionName = null;
+
+                if ($section->action_type == 'product') {
+                    $product = DB::connection('oracle_lmidc')
+                        ->table('to_sfa_products_android')
+                        ->where('product_id', $section->action_id)
+                        ->value('product_ename');
+                    $actionName = $product;
+                } elseif ($section->action_type == 'category') {
+                    $category = DB::connection('oracle_lmidc')
+                        ->table('prod_family')
+                        ->where('family_id', $section->action_id)
+                        ->value('name');
+                    $actionName = $category;
+                }
+
                 return [
                     'id'          => $section->id,
                     'image'       => asset('storage/' . $section->image_path),
                     'action_type' => $section->action_type ?? 'none',
                     'action_id'   => $section->action_id,
+                    'action_name' => $actionName,
                 ];
             });
-
         return response()->json([
             'data' => $sections,
         ], 200);
